eb29b6ea205e56d1c08111864fe31c706a89447f
Maryam Afzal <emmaryam5071@gmail.com>
2025-02-03 19:46:39 +0500

    Add files via upload

---
+++ 33 (2).cpp
@@ -0,0 +1,60 @@
+// switch case
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+    
+    // Display menu
+    cout << "Menu:\n";
+    cout << "1. Addition\n";
+    cout << "2. Subtraction\n";
+    cout << "3. Multiplication\n";
+    cout << "4. Division\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    double num1, num2, result;
+
+    switch (choice) {
+        case 1:
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        
+        case 2:
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        
+        case 3:
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        
+        case 4:
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        
+        default:
+            cout << "Invalid choice! Please enter a number between 1 and 4." << endl;
+    }
+
+    return 0;
+}
