bf25934324400b200ad0fcfa7eaeb3605b5e8df5
Maryam Afzal <emmaryam5071@gmail.com>
2025-02-03 23:39:17 +0500

    Add files via upload

---
+++ 98.cpp
@@ -0,0 +1,21 @@
+//A A A A A
+//B B B B B
+//C C C C C
+//D D D D D
+//E E E E E 
+#include<iostream>
+using namespace std;
+int main(){
+	char i,j;
+	
+	for(int i='A';i<='E';i++)
+	{
+		for(int j='A';j<='E';j++)
+		{
+			cout<<char(i);
+			
+		}
+		cout<<endl;
+	}
+	return 0;
+}
---
+++ 99.cpp
@@ -0,0 +1,23 @@
+//5
+//4 5
+//3 4 5
+//2 3 4 5
+//1 2 3 4 5
+#include<iostream>
+using namespace std;
+int main(){
+	int n=5;
+	for(int i=n;i>=1;i--)
+	{
+	for(int j=i;j<=n;j++)
+	{
+	cout<<j<<" ";
+}
+cout<<endl;
+}
+return 0;
+
+	
+	
+	
+}
