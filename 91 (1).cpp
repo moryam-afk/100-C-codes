956b043c17089982ab3b34945cc70874a50fd229
Maryam Afzal <emmaryam5071@gmail.com>
2025-02-03 23:27:14 +0500

    Add files via upload

---
+++ 91 (1).cpp
@@ -0,0 +1,23 @@
+//1 3 5 7 9
+//3 5 7 9 11 
+//5 7 9 11 13
+//7 9 11 13 15
+//9 11 13 15 17
+#include<iostream>
+using namespace std;
+int main(){
+	int n=5;
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			cout<<(2*(i+j))-3<<" ";
+			
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+			
+		
